95ed79e160a7fac2ea310819de73b2155fef1bf2
halicyafranca <halicyagabrielly@gmail.com>
2026-07-10 09:21:13 -0300

    ainda nao completo

---
+++ src/components/mapa/Lmap.vue
@@ -0,0 +1,19 @@
+
+<template>
+<div style="height: 600px; width: 800px;"> 
+<l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    layer-type="base"
+    name="openstreetmap"></l-tile-layer>
+</l-map>
+</div>
+</template>
+<script setup >
+import { vue } from 'globals';
+import 'leaflet/dist/leaflet.css';
+import {Lmap, LTileLayer} from "@vue-leaflet/vue-leaflet";
+import { ref } from 'vue';
+const zoom= ref(2);
+</script>
+<style scoped>
+</style>
