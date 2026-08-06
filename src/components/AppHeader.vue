cb349bb603e1e1ac52f41d05bd33cf8f2c02403c
giuliamcorrea <marcoscorreagiulia@gmail.com>
2026-08-06 15:44:58 -0300

    #2: html header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import { ref } from 'vue';
+</script>
+
+<template>
+    <header class="header">
+
+        <div class="logo">
+            <img src="#" alt="Logo" />
+            <h1>Abastece Aí</h1>
+        </div>
+
+        <nav>
+            <a href="#">HOME</a>
+            <a href="#">MAPA</a>
+            <a href="#">POSTOS</a>
+            <a href="#">SOBRE NÓS</a>
+        </nav>
+
+        <div class="pesquisa">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" placeholder="Pesquisar"/>
+        </div>
+
+        <a href="#" class="estrela">
+        <i class="fa-solid fa-star"></i>
+        </a>
+
+        <a href="#" class="user">
+        <i class="fa-solid fa-user"></i>
+        </a>
+
+    </header>
+</template>
