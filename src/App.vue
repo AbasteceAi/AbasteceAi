2f00e0c3083aa5100ccb62fda14caf7a3459e6e3
giuliamcorrea <marcoscorreagiulia@gmail.com>
2026-08-13 16:46:34 -0300

    #6 metade pronto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+<body>        
+    <main>
+    <section class="banner">
+    <div class="texto">
+    <h1>Bem Vindo ao <span>Abastece Aí</span></h1>
+    <p>Encontre os postos de gasolina mais baratos pela região de Joinville.</p>
+    <p>Compare os preços em tempo real e economize toda semana.</p>
+    </div>
+    <div class="imagem">
+    <img src="/bonecoPosto.png" alt="boneco" />
+    </div>
+    </section>
+    </main>
+</body>
+</template>
+
+<style scoped>
+.banner {
+    width: 100%;
+    height: 315px;
+    position: relative;
+    background-image: url('/teste3.png');
+    background-size: cover;
+    background-position: center;
+    overflow: hidden;
+}
+
+.texto {
+    position: absolute;
+    left: 60px;
+    top: 50px;
+}
+
+h1 {
+    font-size: 28px;
+    margin-bottom: 30px;
+}
+
+p {
+    font-size: 16px;
+    line-height: 1.5;
+}
+
+.imagem {
+    position: absolute;
+    right: 70px;
+    bottom: 0;
+}
+
+.imagem img {
+    width: 360px;
+    display: block;
+}
+</style>
