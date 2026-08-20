96c585bdbdbb6b84a78266d5f63efd5cb7650c2b
TiagoMPerin <tiagomartinsperin@gmail.com>
2026-08-20 14:48:27 -0300

    adicao dos cards da pagina postos e da funcionalidade de filtro de postos por bairro no postos.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
 import MapaView from '../views/MapaView.vue'
+import PostosView from '../views/PostosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView 
+      component: HomeView
     },
+
     {
       path: '/mapa',
       name: 'mapa',
       component: MapaView
+    },
+
+    {
+      path: '/postos',
+      name: 'postos',
+      component: PostosView
     }
-    
   ]
 })
+
 export default router
